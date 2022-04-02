ec76e357198ee709fda022902dbfa8aa56505f55
ryo0705s <inspire.sasaki15@gmail.com>
2022-04-02 17:38:08 +0900

    特定のシートの中身を出力

--- visitJapan.py
+++ visitJapan.py
@@ -4,5 +4,8 @@ input_file_name = 'since2003_visitor_arrivals.xlsx'
 input_book = pd.ExcelFile(input_file_name)
 input_sheet_name = input_book.sheet_names
 num_sheet = len(input_sheet_name)
-print("sheetの数:", num_sheet)
-print(input_sheet_name)
+input_sheet_df = input_book.parse(input_sheet_name[2])
+print("Sheet name", input_sheet_name[2])
+input_sheet_df.head(10)
+# print("sheetの数:", num_sheet)
+# print(input_sheet_name)
